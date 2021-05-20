852472374034bd79045a1f26a24a8dc745b27e1a
veronica <veroo.9494@gmail.com>
2021-05-20 14:27:31 -0300

    customers2

--- src/main/java/com/automotora/ventas/service/CustomerServiceImp.java
+++ src/main/java/com/automotora/ventas/service/CustomerServiceImp.java
@@ -11,11 +11,11 @@ public class CustomerServiceImp implements CustomerService {
 
     @Override
     public ArrayList<Customer> getCustomers() {
-        Customer custumer1 = new Customer(1, "Pepe", "pepe@gmail.com");
-        Customer custumer2 = new Customer(2, "Pepe2", "pepe2@gmail.com");
+        Customer firstCustumer = new Customer(1, "Pepe", "pepe@gmail.com");
+        Customer secondCustumer = new Customer(2, "Pepe2", "pepe2@gmail.com");
         ArrayList<Customer> customers = new ArrayList<Customer>();
-        customers.add(custumer1);
-        customers.add(custumer2);
+        customers.add(firstCustumer);
+        customers.add(secondCustumer);
         return customers;
     }
 }
